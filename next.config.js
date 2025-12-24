b34fcf9a5ef769adaf54f4add82f0fa4b453a83a
makto-toruk <manasij@hey.com>
2025-12-24 11:57:15 -0800

    Update Next.js to 16.1.1 to fix security vulnerability

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias["@"] = __dirname;
-    return config;
-  },
+  // Turbopack is enabled by default in Next.js 16
+  turbopack: {},
 };
 
 export default nextConfig;
